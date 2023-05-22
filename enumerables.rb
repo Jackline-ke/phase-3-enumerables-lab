62895da2a805f37ef62b2fb0272dabb87b5d1866
Jackline-ke <antoninahodhiambo@gmail.com>
2023-05-22 17:39:08 +0300

    Completed assignment

--- enumerables.rb
+++ enumerables.rb
@@ -13,12 +13,17 @@ end
 # with the names of each spicy food
 def get_names(spicy_foods)
   # your code here
+
+  spicy_foods.map{ |myName| "#{myName[:name]}" }
 end
 
 # given an array of spicy foods, **return an array of hashes** 
 # where the heat level of the food is greater than 5
 def spiciest_foods(spicy_foods)
   # your code here
+
+  spicy_foods.filter { |spice| spice[:heat_level] > 5}
+
 end
 
 # given an array of spicy foods, **output to the terminal**
@@ -28,18 +33,30 @@ end
 # "hello" * 3 == "hellohellohello"
 def print_spicy_foods(spicy_foods)
   # your code here
+
+  spicy_foods.map do |spice|
+  puts "#{spice[:name]} (#{spice[:cuisine]}) | Heat Level: #{'🌶' * (spice[:heat_level])}"
+  end
 end
 
 # given an array of spicy foods and a string representing a cuisine, **return a single hash**  
 # for the spicy food whose cuisine matches the cuisine being passed to the method
 def get_spicy_food_by_cuisine(spicy_foods, cuisine)
   # your code here
+
+  spicy_foods.find do |spice|
+    spice[:cuisine] == cuisine
+  end
 end
 
 # Given an array of spicy foods, **return an array of hashes** 
 # sorted by heat level from lowest to highest
 def sort_by_heat(spicy_foods)
   # your code here
+
+  spicy_foods.sort do |food1, food2|
+  food1[:heat_level] <=> food2[:heat_level]
+  end
 end
 
 # given an array of spicy foods, output to the terminal ONLY 
@@ -48,10 +65,18 @@ end
 # HINT: Try to use methods you've already written to solve this!
 def print_spiciest_foods(spicy_foods)
   # your code here
+
+  spicy_foods.map do |food|
+      if "#{food[:heat_level]}".to_i > 5
+        emojis = "🌶"*"#{food[:heat_level]}".to_i
+    puts "#{food[:name]} (#{food[:cuisine]}) | Heat Level: #{emojis}"
+    end
 end
 
 # given an array of spicy foods, return an integer representing 
 # the average heat level of all the spicy foods in the array
 def average_heat_level(spicy_foods)
   # your code here
+  spicy_foods.map{ |myName| "#{myName[:heat_level]}".to_i }.sum/spicy_foods.map{ |myName| "#{myName[:heat_level]}".to_i }.size
+  end
 end
